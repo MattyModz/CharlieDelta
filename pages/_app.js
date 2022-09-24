b35d534d3d33fc7d0b831fdea0df03c6d44cf78a
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-24 17:52:02 +0100

    first

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../src/styles/index.scss";
 import { AnimatePresence } from "framer-motion";
 import Footer from "../src/componants/Footer/footer";
 import Header from "../src/componants/Header/header";
-import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps, router }) {
   // useEffect(() => {
